f6548cd24fd88fa14afed874c848a90a1034a876
villevt <ville.turku99@gmail.com>
2022-07-23 16:21:56 +0300

    Init repo

---
+++ nlp/model_trainer.py
@@ -0,0 +1,135 @@
+import argparse
+import joblib
+from math import ceil, floor
+import numpy as np
+import os
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin, clone
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import ComplementNB, GaussianNB, MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
+import textutils
+
+class Model(BaseEstimator, ClassifierMixin, TransformerMixin):
+    def __init__(self, feature_extractor=CountVectorizer(), estimator=ComplementNB(), ngram_range=(1, 1)):
+        """ 
+        Inits the model.
+        Arguments:
+            feature_extractor: Method to use for transforming feature extraction. Supports some sklearn extractors:
+                - CountVectorizer()
+                - TfidfVectorizer
+            estimator: sklearn estimator to use. Options:
+                - ComplementNB()
+                - GaussianNB()
+                - MultinomialNB()
+                - LinearSVC()
+                - SGDClassifier()
+            ngram_range: Range of ngrams to produce as features
+        """
+
+        self.feature_extractor = feature_extractor
+        self.ngram_range = ngram_range
+
+        self.estimator = estimator
+
+    def fit(self, X, y):
+        """
+        Fits the the model.
+        Arguments:
+            X:  Input data
+            y:  Input labels
+        """
+        self.feature_extractor.set_params(tokenizer=textutils.clean_text, ngram_range=self.ngram_range)
+
+        X_processed = self.feature_extractor.fit_transform(X.copy()).toarray()
+        self.estimator = self.estimator.fit(X_processed, y)
+
+    def score(self, X, y):
+        """
+        Evaluates the accuracy of the data.
+        Arguments:
+            - X: Real labels
+            - y: Real labels
+
+        Returns:
+            Balanced accuracy score of predicted labels
+        """
+
+        return balanced_accuracy_score(y, self.predict(X))
+
+    def predict(self, X):
+        """
+        Generates predictions based on data.
+        Arguments:
+            X: (unseen) data
+        """
+
+        X = self.feature_extractor.transform(X.copy()).toarray()
+        y = self.estimator.predict(X)
+        return y
+
+
+def load_data(datasets, train_size=0.8):
+    """
+    Loads a dataset for training the model.
+    Arguments:
+        datasets:   Datasets to use in training. Takes in tab-delimited text files with sentences on first column and binary sentiment on the second.
+        partition:  Should the dataset be partitioned, i.e. should the entire data be loaded into memory at once?
+        train_size: Size of training data set as a fraction of the dataset
+    """
+    # Load data
+    data = np.concatenate(
+        [np.loadtxt(d, dtype=str, delimiter="\t") for d in datasets])
+
+    # Shuffle data
+    np.random.default_rng().shuffle(data)
+
+    # Split into training and testing data
+    trainIdx = floor(data.shape[0] * train_size)
+    train = data[:trainIdx, :]
+    test = data[trainIdx:, :]
+
+    # Set X and y
+    X_train, y_train = train[:, 0], train[:, 1].astype(int)
+    X_test, y_test = test[:, 0], test[:, 1].astype(int)
+
+    return X_train, X_test, y_train, y_test
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", nargs="+",
+                        help="list of .txt files to use in training")
+    parser.add_argument("-d", nargs="?", default="/data/",
+                        help="directory for training data files")
+    parser.add_argument("-s", nargs="?", default=0.8,
+                        help="train set size")
+
+    args = parser.parse_args()
+
+    # Load datasets from the given folder and files
+    datasets = [os.getcwd() + args.d + f for f in args.f]
+
+    # Initialize model
+    model = Model()
+    X_train, X_test, y_train, y_test = load_data(
+        datasets, train_size=float(args.s))
+
+
+    gs = GridSearchCV(Model(), param_grid={
+        "feature_extractor": [CountVectorizer(), TfidfVectorizer()],
+        "estimator": [ComplementNB(), GaussianNB(), MultinomialNB(), LinearSVC(), SGDClassifier()],
+        "ngram_range": [(1, 1), (1, 2), (1, 3)]}, verbose=1)
+    gs.fit(X_train, y_train)
+
+    print(gs.cv_results_)
+
+    print(gs.best_estimator_)
+
+    print(f"Selected parameters: {gs.best_params_}")
+    print(f"Mean score on validation set: {gs.best_score_}")
+    print(f"Balanced accuracy score on test set of {ceil((1 - float(args.s)) * 100)}%: {gs.score(X_test, y_test)}")
+
+    joblib.dump(gs.best_estimator_,  "sentiment_classifier.joblib.pkl")
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Sentimentai
+
+## Summary
+
+A small hobby project for practicing NLP sentiment analysis, and more so ML deployment. The intention is to create 
+* Command-line script for (re-)training supervised models based on grid search defined in the script itself (scikit-learn)
+* Small full-stack webpage for demonstrating inference from the model (Django, React)
+* Deployment pipeline, for example to Heroku
+
+## Command-line script
+
+The command line script for model deployment is located in the `nlp` folder. The script takes in text files in the following format for training
+
+    negative text\t0\n
+    positive text\t0\n
+    ...
+
+The script then picks a best model out of multiple options, based on balanced accuracy score. Once the best model is selected, it is dumped into file `model.joblib`.
+
+The command line script expects the following syntax:
+
+    python model_trainer.py
+    -f  List of text files to use in training, relative to working or specified directory
+    -d  Directory to search for text files, if other than working directory
+    -s  Size of training test, as fraction of total dataset    
+
+## Modelling approach
+
+The modelling approach here was relatively simple. I employed an grid search over multiple different feature extractors and classifiers, and chose the classifier with the best balanced accuracy (since I haven't done a stratified train-test-split!) 
+
+I represented the features with:
+- Bag of words, 1 to 3 n-grams
+- TF-IDF, 1 to 3 n-grams
+
+Different classifiers used were:
+- Complement Naive Bayes
+- Gaussian Naive Bayes
+- Multinomial Naive Bayes
+- Linear Support Vector Classifier
+- Stochastic Gradient Descent Classifier
+
+Based on my results so far, it seems that Linear SVC is the best approach, together with TF-IDF, and mono- and bi-gram features. The model exhibited a validation balanced accuracy of 80.30% on 1 800 observations' 5-fold CV, and a test balanced accuracy of 80.79% on a test set of 1 200 observations.
+
+Note that we are only comparing different models here with default hyperparameters, and not getting deep into hyperparameter-tuning.
+
+## Model data
+
+I constructed the model on sentiment data (`nlp/data`) derived from https://www.kaggle.com/datasets/marklvl/sentiment-labelled-sentences-data-set
+
+The original dataset is attributed to _'From Group to Individual Labels using Deep Features', Kotzias et. al,. KDD 2015_
+
+The data consists of 500 positive-labeled (1) and 500 negative-lableled (0) reviews from Amazon, IMDB and Yelp each. The reviews and labels are tab-delimited, with observations delimited by linebreaks.
+
+Obviously, the dataset is focused on reviews and might hence not be fully generalizable to communication in other contexts. Furthermore, the datasets are quite small. To better assess (and train) the generality of these models, data from different type of sources (e.g. literature, instant messaging) should be obtained.
